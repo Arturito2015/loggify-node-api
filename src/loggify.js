d59dd5da6eebdd503111b5638342cb2c61a2d82f
Guido Faranna <farannaguido@gmail.com>
2020-12-18 17:39:16 -0300

    cambios en la inicializacion de la api

--- src/loggify.js
+++ src/loggify.js
@@ -3,19 +3,19 @@ const axios = require('axios');
 const url = 'http://104.248.103.156:1339/loggify'
 
 class Loggify {
-  constructor(workspaceId,workspaceCode,workspaceName,hashCode) {
-      this.workspaceCode = workspaceCode;
-      this.workspaceName = workspaceName;
-      this.workspaceId = workspaceId;
-      this.hashCode = hashCode;
+
+  static initialize(workspaceId,workspaceCode,workspaceName,hashCode){
+    this.workspaceCode = workspaceCode;
+    this.workspaceName = workspaceName;
+    this.workspaceId = workspaceId;
+    this.hashCode = hashCode;
   }
 
-  async log(title,message,estado = "error",grupo = '') {
+  static async log(title,message,estado = "error",grupo = '') {
     const params = {workspaceId:this.workspaceId,workspaceCode:this.workspaceCode,workspaceName:this.workspaceName,hashCode:this.hashCode,title,message,estado,grupo}
     const response = await axios.post(url,params);
     return response.data
   }
-
 
 }
 
